dfa52a16238f3efbd814f66c9f7c02ed1bd24e28
Abebe asnake <abexasnex@gmail.com>
2021-07-21 19:24:18 -0400

    Add reminder UI created

--- src/components/AddReminder/AddReminder.tsx
+++ src/components/AddReminder/AddReminder.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import CloseIcon from '@material-ui/icons/Close';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -7,6 +7,9 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { WithStyles, withStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { TextField, Button  } from '@material-ui/core';
+import { SketchPicker } from 'react-color';
+import reactCSS from 'reactcss';
 
 const styles = (theme: Theme) => createStyles({
 	addReminderFormContainer: {
@@ -19,7 +22,19 @@ const styles = (theme: Theme) => createStyles({
 		position: 'absolute',
 		right: '10px',
 		top: '10px'
+	},
+	addNote:{
+		marginBottom:'10px'
+	},
+	saveButton:{
+		marginTop:'50px',
+		float: 'right'
+		
+	},
+	sketchPicker:{
+		marginTop: "50px"
 	}
+      
 });
 
 interface Props extends WithStyles<typeof styles>{
@@ -28,6 +43,44 @@ interface Props extends WithStyles<typeof styles>{
 }
 
 const AddReminder = (props: Props) => {
+	const[displayColorPicker, displayColor] = useState(false);
+	const[color, setColor] = useState({
+      r: '241',
+      g: '112',
+      b: '19',
+      a: '1'
+    });
+const styles = reactCSS({
+      'default': {
+        color: {
+          width: '38px',
+          height: '18px',
+          borderRadius: '2px',
+          background: `rgba(${color.r }, ${color.g }, ${color.b }, ${color.a })`,
+        },
+        swatch: {
+          padding: '2px',
+		  marginTop: '29px',
+		  marginLeft: '10px',
+          background: '#fff',
+          borderRadius: '1px',
+          boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+          display: 'inline-block',
+          cursor: 'pointer',
+        },
+        popover: {
+    position: 'relative',
+          zIndex: '2',
+        },
+        cover: {
+          position: 'fixed',
+          top: '0px',
+          right: '0px',
+          bottom: '0px',
+          left: '0px',
+        },
+      },
+    });
 		const { classes, isOpen, onClose } = props;
 
 		return (
@@ -47,7 +100,36 @@ const AddReminder = (props: Props) => {
 				<Divider light />
 				<DialogContent className={ classes.addReminderFormContainer }>
 					<Typography>
-						Use this space to create the UI to add a reminder to the calendar.
+						  <TextField className ={classes.addNote}  id="standard-basic" label="Add Note(Maximum of 30 character)" fullWidth = {true} />
+						<form noValidate>
+  <TextField
+    id="date"
+    label="date"
+       type="date"
+    InputLabelProps={{
+      shrink: true,
+    }}
+  />
+
+  <TextField
+    id="time"
+    label="Time"
+	type="time"
+    defaultValue="07:30"
+    InputLabelProps={{
+      shrink: true,
+    }}
+    
+  />
+        <div style={ styles.swatch } onClick={ () => displayColor(!displayColorPicker) }>
+          <div style={ styles.color } />
+        </div>
+        { displayColorPicker ? <div style={ styles.popover }>
+          <div style={ styles.cover } onClick={() => displayColor(false)}/>
+          <SketchPicker color={ color } onChange={(color) => setColor(color.rgb) } />
+        </div> : null }
+
+</form> <Button className={classes.saveButton} variant="outlined">Save</Button>
 					</Typography>
 				</DialogContent>
 			</Dialog>
